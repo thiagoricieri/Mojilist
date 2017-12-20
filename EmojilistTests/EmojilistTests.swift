af83a436f3e7ce3d668ca5023ea9b6d6ae5d917a
Thiago Ricieri <thiago.ricieri@gmail.com>
2017-12-20 12:00:05 -0200

    Initial Commit

---
+++ EmojilistTests/EmojilistTests.swift
@@ -0,0 +1,36 @@
+//
+//  EmojilistTests.swift
+//  EmojilistTests
+//
+//  Created by Thiago Ricieri on 20/12/2017.
+//  Copyright © 2017 Ghost Ship. All rights reserved.
+//
+
+import XCTest
+@testable import Emojilist
+
+class EmojilistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
